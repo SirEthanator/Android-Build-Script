11bbb28a4a3ce7f05881d050e22f5c6efc2b7bc2
Ethan <e.slimerancher@gmail.com>
2024-09-24 17:09:32 +1200

    Fix signing by using modified make_key script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Sync Everest sources and add local manifest
+echo '==== Beginning sync ===='
 rm -rf .repo/local_manifests
 repo init -u https://github.com/ProjectEverest/manifest -b 14 --git-lfs
 git clone https://github.com/SirEthanator/bluejay_local_manifest.git --depth 1 .repo/local_manifests
@@ -10,7 +11,13 @@ echo '======================='
 echo '==== Sync complete ===='
 echo '======================='
 
+
 # Signing
+echo '==== Replacing make_key script ===='
+rm -f development/tools/make_key
+curl https://raw.githubusercontent.com/SirEthanator/development_tools_make_key/refs/heads/main/make_key > development/tools/make_key
+
+echo '==== Beginning signing ===='
 subject="/C=$COUNTRY/ST=$STATE/L=$CITY/O=$NAME/OU=$NAME/CN=$NAME/emailAddress=$EMAIL"
 for x in releasekey platform shared media networkstack verity otakey testkey cyngn-priv-app sdk_sandbox bluetooth verifiedboot nfc; do
     ./development/tools/make_key vendor/lineage/signing/keys/$x "$subject"
@@ -21,9 +28,14 @@ echo '=========================='
 echo '==== Signing complete ===='
 echo '=========================='
 
+
 # Build
-echo 'Starting build'
+echo '==== Starting build ===='
 . build/envsetup.sh
 lunch lineage_bluejay-user
 mka everest
+
+echo '========================'
+echo '==== Build complete ===='
+echo '========================'
 
