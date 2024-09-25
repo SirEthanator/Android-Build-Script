1f322eee022734772c6de3ed9a48006739889c62
Ethan <e.slimerancher@gmail.com>
2024-09-25 16:50:28 +1200

    Disable signing and fix ccache installation
    Disabling signing to determine if that is why my builds are failing. ccache should hopefully install correctly now too.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install ccache
-sudo apt install ccache
+sudo apt update; sudo apt install -y ccache
 
 
 # Sync Everest sources and add local manifest
@@ -17,20 +17,20 @@ echo '======================='
 
 
 # Signing
-echo '==== Replacing make_key script ===='
-rm -f development/tools/make_key
-curl https://raw.githubusercontent.com/SirEthanator/development_tools_make_key/refs/heads/main/make_key > development/tools/make_key
-chmod +x development/tools/make_key
-
-echo '==== Beginning signing ===='
-subject="/C=$COUNTRY/ST=$STATE/L=$CITY/O=$NAME/OU=$NAME/CN=$NAME/emailAddress=$EMAIL"
-for x in releasekey platform shared media networkstack verity otakey testkey cyngn-priv-app sdk_sandbox bluetooth verifiedboot nfc; do
-    ./development/tools/make_key vendor/lineage/signing/keys/$x "$subject"
-done
-
-echo '=========================='
-echo '==== Signing complete ===='
-echo '=========================='
+# echo '==== Replacing make_key script ===='
+# rm -f development/tools/make_key
+# curl https://raw.githubusercontent.com/SirEthanator/development_tools_make_key/refs/heads/main/make_key > development/tools/make_key
+# chmod +x development/tools/make_key
+#
+# echo '==== Beginning signing ===='
+# subject="/C=$COUNTRY/ST=$STATE/L=$CITY/O=$NAME/OU=$NAME/CN=$NAME/emailAddress=$EMAIL"
+# for x in releasekey platform shared media networkstack verity otakey testkey cyngn-priv-app sdk_sandbox bluetooth verifiedboot nfc; do
+#     ./development/tools/make_key vendor/lineage/signing/keys/$x "$subject"
+# done
+#
+# echo '=========================='
+# echo '==== Signing complete ===='
+# echo '=========================='
 
 
 # Build
@@ -38,8 +38,4 @@ echo '==== Starting build ===='
 . build/envsetup.sh
 lunch lineage_bluejay-user
 mka everest
-
-echo '========================'
-echo '==== Build complete ===='
-echo '========================'
 
