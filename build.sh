698e3031b5402ba020b7f1311e7b90bcecb4b0fd
Ethan <e.slimerancher@gmail.com>
2024-09-26 18:51:27 +1200

    Remove signing
    Signing was causing builds to fail so it has now been removed from the script

--- build.sh
+++ build.sh
@@ -16,23 +16,6 @@ echo '==== Sync complete ===='
 echo '======================='
 
 
-# Signing
-# echo '==== Replacing make_key script ===='
-# rm -f development/tools/make_key
-# curl https://raw.githubusercontent.com/SirEthanator/development_tools_make_key/refs/heads/main/make_key > development/tools/make_key
-# chmod +x development/tools/make_key
-#
-# echo '==== Beginning signing ===='
-# subject="/C=$COUNTRY/ST=$STATE/L=$CITY/O=$NAME/OU=$NAME/CN=$NAME/emailAddress=$EMAIL"
-# for x in releasekey platform shared media networkstack verity otakey testkey cyngn-priv-app sdk_sandbox bluetooth verifiedboot nfc; do
-#     ./development/tools/make_key vendor/lineage/signing/keys/$x "$subject"
-# done
-#
-# echo '=========================='
-# echo '==== Signing complete ===='
-# echo '=========================='
-
-
 # Build
 echo '==== Starting build ===='
 . build/envsetup.sh
